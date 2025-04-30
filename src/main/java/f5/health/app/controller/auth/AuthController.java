212e2214fd1ac4dd944adf9ad4b9ff1f64a235a9
ToJongHyoMo <kwn07012@naver.com>
2025-04-30 14:05:09 +0900

    second commit

--- src/main/java/f5/health/app/controller/auth/AuthController.java
+++ src/main/java/f5/health/app/controller/auth/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class AuthController {
+public class gAuthController {
 
 //    @PostMapping("/login")
 //    JwtResponse...

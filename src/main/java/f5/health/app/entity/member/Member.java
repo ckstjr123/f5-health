15239e0f7f57e6342f94c0006e47890600d45e52
ckstjr <113373305+ckstjr@users.noreply.github.com>
2025-05-28 22:17:15 +0900

    건강 물품 프롬포트, 흡연 최대량 수정

--- src/main/java/f5/health/app/entity/member/Member.java
+++ src/main/java/f5/health/app/entity/member/Member.java
@@ -186,7 +186,7 @@ public class Member extends BaseTimeEntity {
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class MemberCheckUp {
 
-        public static final int DAILY_MAX_CIGARETTES = 35;
+        public static final int DAILY_MAX_CIGARETTES = 40;
 
         @Schema(description = "생년월일", example = "2000-04-18", requiredMode = REQUIRED)
         @NotNull(message = "생년월일을 입력해주세요.")
